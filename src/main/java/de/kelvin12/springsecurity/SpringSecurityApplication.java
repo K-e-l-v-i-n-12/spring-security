9c192f51211cc95b9139c4b5b045730e690c5e3d
K-e-l-v-i-n-12 <105751624+K-e-l-v-i-n-12@users.noreply.github.com>
2024-11-26 21:56:57 +0100

    init

---
+++ src/main/java/de/kelvin12/springsecurity/SpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package de.kelvin12.springsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringsecurityApplication.class, args);
+	}
+
+}
